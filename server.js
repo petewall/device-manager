8cbac0b56ecb19578948a3a103041f677582ad56
Pete Wall <pete@petewall.net>
2022-12-28 20:34:38 -0600

    Add linting and more work on the ui
    Signed-off-by: Pete Wall <pete@petewall.net>

--- server.js
+++ server.js
@@ -4,26 +4,29 @@ const fastify = require('fastify')({ logger: true })
 const fastifyStatic = require('@fastify/static')
 const path = require('path')
 
+const port = process.env.PORT || 3000
+// const deviceService = process.env.DEVICE_SERVICE || 'localhost:3001'
+// const firmwareService = process.env.FIRMWARE_SERVICE || 'localhost:3002'
+
 fastify.register(fastifyStatic, {
-    root: path.join(__dirname, 'public'),
+  root: path.join(__dirname, 'public'),
 })
 
 fastify.get('/', function (req, reply) {
-    reply.sendFile('index.html')
+  reply.sendFile('index.html')
 })
 
 fastify.register(fastifyStatic, {
-    root: path.join(__dirname, 'node_modules'),
-    prefix: '/lib/',
-    decorateReply: false, // the reply decorator has been added by the first plugin registration
-    index: false,
-
+  root: path.join(__dirname, 'node_modules'),
+  prefix: '/lib/',
+  decorateReply: false,   // the reply decorator has been added by the first plugin registration
+  index: false,           // Disallow browsing of node_modules
 })
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 })
+    await fastify.listen({ port })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
